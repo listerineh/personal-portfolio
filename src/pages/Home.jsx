39b23b0451af9035acb59857a84af6467d02cdca
listerineh <jsalvarez4@espe.edu.ec>
2022-06-17 11:08:53 -0500

    Add: New technology

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,6 +21,7 @@ const Home = () => {
     { name: "OpenCV", url: "https://opencv.org/" },
     { name: "Tensorflow", url: "https://www.tensorflow.org/" },
     { name: "Keras", url: "https://keras.io/" },
+    { name: "Django", url: "https://www.djangoproject.com/" },
     { name: "Node", url: "https://nodejs.org/en/" },
     { name: "React", url: "https://reactjs.org/" },
     { name: "Tailwind", url: "https://tailwindcss.com/" },
