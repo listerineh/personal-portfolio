0815b1b21360669b872ff82a2a640aa0fdba3ac2
Sebastian Alvarez <jsalvarez4@espe.edu.ec>
2022-09-06 20:06:10 -0500

    refactor: experiences page to the actual time

--- src/data/experiences.js
+++ src/data/experiences.js
@@ -1,13 +1,42 @@
 export const experiences = [
   {
-    title: "Student / Developer",
-    location: "Military Applications Research Center CICTE",
-    url: "https://cicte.espe.edu.ec/",
-    timerange: "Jul. 2020 - Dec. 2020",
+    title: "Software Developer",
+    location: "ioet",
+    url: "https://ioet.com/",
+    timerange: "Jul. 2022 - Present",
     functions: [
-      { body: "Development of computer vision algorithms in Matlab." },
-      { body: "Integration with Arduino and servomotors." },
-      { body: "Presentation of the project to a jury." },
+      {
+        body: "Develop of a Slack bot for an internal app using Python.",
+      },
+      {
+        body: "Administration and help in the database migration process from Cosmos DB to PostgreSQL.",
+      },
+      {
+        body: "Help in the backend migration process and in the correct documentation of it.",
+      },
+      {
+        body: "Work as a frontend developer using React ts with material UI and React Admin.",
+      },
+      {
+        body: "Help in the develop of the authentication/login of an internal app.",
+      },
+    ],
+  },
+  {
+    title: "Information Technology Intern",
+    location: "ioet",
+    url: "https://ioet.com/",
+    timerange: "Apr. 2022 - Jul. 2022",
+    functions: [
+      {
+        body: "Maintenance of an internal application using: Python, Docker, Azure Platform, Angular, Git.",
+      },
+      { body: "Training in Scrum agile development methodology." },
+      { body: "English training for communications with foreign clients." },
+      { body: "Management of PostgreSQL and CosmosDB databases." },
+      {
+        body: "Training in DevOps with Terraform, Github actions and CI/CD based on an Azure architecture.",
+      },
     ],
   },
   {
@@ -17,21 +46,23 @@ export const experiences = [
     timerange: "Jan. 2022 - Mar. 2022",
     functions: [
       { body: "Management of the e-commerce website with Wordpress." },
-      { body: "Meetings for communicating progress to the administratives of the project." },
-      { body: "Use plugins like: WooCommerce, Elementor, Dokan, Google Analytics, etc." },
+      {
+        body: "Meetings for communicating progress to the administratives of the project.",
+      },
+      {
+        body: "Use plugins like: WooCommerce, Elementor, Dokan, Google Analytics, etc.",
+      },
     ],
   },
   {
-    title: "Information Technology Intern",
-    location: "ioet",
-    url: "https://ioet.com/",
-    timerange: "Apr. 2022 - Present",
+    title: "Student / Developer",
+    location: "Military Applications Research Center CICTE",
+    url: "https://cicte.espe.edu.ec/",
+    timerange: "Jul. 2020 - Dec. 2020",
     functions: [
-      { body: "Maintenance of an internal application using: Python, Docker, Azure Platform, Angular, Git." },
-      { body: "Training in Scrum agile development methodology." },
-      { body: "English training for communications with foreign clients." },
-      { body: "Management of PostgreSQL and CosmosDB databases." },
-      { body: "Training in DevOps with Terraform, Github actions and CI/CD based on an Azure architecture." },
+      { body: "Development of computer vision algorithms in Matlab." },
+      { body: "Integration with Arduino and servomotors." },
+      { body: "Presentation of the project to a jury." },
     ],
   },
 ];
