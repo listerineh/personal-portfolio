45fe17356c2ba5cf2ce44c10d46fd5889fa3f5b9
Sebastian Alvarez <jsalvarez4@espe.edu.ec>
2022-09-06 20:06:51 -0500

    refactor: petvet project info

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,26 +1,31 @@
-import VeterinaryPhoto from "../images/veterinary-admin.webp";
 import BudgetPhoto from "../images/budget-planner.webp";
 import ClientsPhoto from "../images/clients-admin.webp";
 import CryptosPhoto from "../images/quote-cryptos.webp";
 import SignLanguageMenu from "../images/menu-sign-language.webp";
 import SignLanguage1 from "../images/1-sign-language.webp";
 import SignLanguage2 from "../images/2-sign-language.webp";
+import Petvet1 from "../images/petvet1.webp";
+import Petvet2 from "../images/petvet2.webp";
+import Petvet3 from "../images/petvet3.webp";
+import Petvet4 from "../images/petvet4.webp";
+import Petvet5 from "../images/petvet5.webp";
 
 export const projects = [
   {
     title: "Veterinary Administrator",
-    photo: [VeterinaryPhoto],
+    photo: [Petvet1, Petvet2, Petvet3, Petvet4, Petvet5],
     url: "1",
     description:
-      "Simple responsive web application to manage a veterinary using CRUD operations and saving the data in Local Storage (Browser).",
+      "Simple responsive web application to manage a veterinary using CRUD operations and saving the data in Local Storage (Browser) with a beauty dark mode.",
     stack: [
-      "vite", 
-      "reactjs", 
-      "react-hooks", 
-      "react-components", 
-      "tailwindcss"
+      "vite",
+      "reactjs",
+      "react-hooks",
+      "react-components",
+      "tailwindcss",
+      "vercel",
     ],
-    demo: "https://veterinary-administrator.netlify.app/",
+    demo: "https://petvet-admin.vercel.app/",
     repo: "https://github.com/listerineh/veterinary-administrator",
   },
   {
@@ -68,15 +73,15 @@ export const projects = [
     url: "4",
     description:
       "Simple responsive web application to quote currencies instantly with the TOP 12 cryptocurrencies of the moment.",
-      stack: [
-        "vite",
-        "react",
-        "react-hooks",
-        "react-custom-hooks",
-        "react-components",
-        "styled-components",
-        "google-fonts",
-      ],
+    stack: [
+      "vite",
+      "react",
+      "react-hooks",
+      "react-custom-hooks",
+      "react-components",
+      "styled-components",
+      "google-fonts",
+    ],
     demo: "https://quote-cryptos.netlify.app",
     repo: "https://github.com/listerineh/quote-cryptocurrencies",
   },
@@ -86,13 +91,7 @@ export const projects = [
     url: "5",
     description:
       "Platform for learning sign language through interactive training through the computer, the BETA version has a way to test what has been learned.",
-      stack: [
-        "python",
-        "pyqt5",
-        "tensorflow",
-        "keras",
-        "opencv",
-      ],
+    stack: ["python", "pyqt5", "tensorflow", "keras", "opencv"],
     repo: "https://github.com/listerineh/alphabet-sign-languaje-recognition",
   },
 ];
