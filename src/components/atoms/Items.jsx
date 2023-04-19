00c8c6a8bd5bd1fa329fd7d3c1dd81257887b1f8
listerineh <jsalvarez4@espe.edu.ec>
2023-04-19 01:51:24 -0500

    remove: unused commentary

--- src/components/atoms/Items.jsx
+++ src/components/atoms/Items.jsx
@@ -1,33 +1,5 @@
 const Items = ({ list }) => {
   return (
-    // <aside className="text-white py-10 pl-10 pr-10 cursor-default">
-    //   {list.map((data) => (
-    //     <div key={data.timerange} className="flex flex-row">
-    //       <span className="w-1 bg-zinc-800">
-    //         <span className="flex mr-7 mt-8 w-4 h-2 bg-zinc-800"></span>
-    //       </span>
-    //       <div className="flex flex-col py-5 ml-5">
-    //         <h2 className="text-2xl md:text-3xl font-bold">{data.title}</h2>
-    //         <h3 className="font-thin text-indigo-500">{data.location}</h3>
-    //         <p className="font-thin text-sm">{data.timerange}</p>
-    //         <p className="font-thin text-xs">{data.place}</p>
-    //         {data.subtitle && (
-    //           <div className="mt-2">
-    //             <h3 className="font-semibold">{data.subtitle}:</h3>
-    //             {data.extras.map((extra) => (
-    //               <p
-    //                 key={extra}
-    //                 className="pl-5 font-thin text-sm text-justify"
-    //               >
-    //                 - {extra}
-    //               </p>
-    //             ))}
-    //           </div>
-    //         )}
-    //       </div>
-    //     </div>
-    //   ))}
-    // </aside>
     <article className="text-white p-10 cursor-default">
       {list.map((item) => (
         <div
