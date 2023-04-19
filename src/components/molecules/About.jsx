2fd1cac230b0212cfafee523a442cd976505b894
listerineh <jsalvarez4@espe.edu.ec>
2023-04-19 01:57:52 -0500

    refactor: spacing in mobile

--- src/components/molecules/About.jsx
+++ src/components/molecules/About.jsx
@@ -4,7 +4,7 @@ import SocialMedia from "../atoms/SocialMedia";
 
 const About = () => {
   return (
-    <article className="md:px-28 px-10 mt-10 cursor-default max-w-7xl">
+    <article className="md:px-28 px-10 mt-20 md:mt-10 cursor-default max-w-7xl">
       <div className="flex flex-col md:flex-row justify-center items-center text-white pb-5 md:pb-0">
         <aside className="w-full md:w-2/3 p-5 md:p-10">
           <p className="md:text-xl text-md font-extralight ">
