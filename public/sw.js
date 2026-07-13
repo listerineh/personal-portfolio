5ba3ff8bd750b3569b78a40f3011b0924e7465a9
listerineh <sebask8er.alvarez@gmail.com>
2026-07-13 02:19:19 -0500

    feat: adding skills

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'listerineh-portfolio-2026-07-13T06-46-59';
+const CACHE_NAME = 'listerineh-portfolio-2026-07-13T06-53-15';
 const urlsToCache = [
   '/offline.html',
   '/manifest.json',
